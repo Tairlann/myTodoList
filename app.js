04163859ded932145153a21b488d8d6b41bd3ab5
Tairlan Kuralbek <four44488@gmail.com>
2021-07-12 18:26:48 +0200

    created 'close' span element to each future li element

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ function press(event) {
     }
     document.getElementById("inp").value = "";
   }
+
+  //create 'x' span on each list element
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.appendChild(txt);
+  span.className = "close";
+  li.appendChild(span);
 }
